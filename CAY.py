1ae817554460cfc5013bc87ccc997e180089dbf1
Charles-Yorke25 <2425402246@live.gctu.edu.gh>
2026-05-30 22:54:54 +0000

    Add files via upload

---
+++ CAY.py
@@ -0,0 +1,31 @@
+# File: CAY.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: KORSAH YORKE CHARLES ALLEN
+# STUDENT ID:  2425402246
+# Email: 2425402246@gctu.edu.gh
+# Grader: AUGUSTUS
+# On my honor, KORSAH YORKE CHARLES ALLEN, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials CAY in small and large stylized block letters.
+    print()
+    print("...CAY")
+    print()
+    print("....CCCCCCCC......****....AAAA.......****YY........YY...****")
+    print("...CCC............****...AA.AA.......****.YY......YY....****")
+    print("...CC.............****..AA..AA.......****..YY....YY.....****")
+    print("...CC.............****.AA...AA.......****...YY..YY......****")
+    print("...CC.............****AA....AA.......****....YYYY.......****")
+    print("...CC.............****AAAAAAAAAAAA...****.....YY........****")
+    print("...CC.............****AA....AA.......****.....YY........****")
+    print("...CC.............****AA....AA.......****.....YY........****")
+    print("...CCC............****AA....AA.......****.....YY........****")
+    print("....CCCCCCCC......****AA....AA.......****.....YY........****")
+    print()
+
+
+main()
